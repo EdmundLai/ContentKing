d4ebee40c4cd7193c4fe6ddf368ada3dafeb278b
Edmund Lai <edmund.lai.07@gmail.com>
2020-12-03 18:08:46 -0800

    added responsive side menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 //import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
-import ContentFeed from "./components/ContentFeed/ContentFeed";
+
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
+
+import ResponsiveDrawer from "./components/ResponsiveDrawer/ResponsiveDrawer";
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
       <Container minWidth="sm">
-        <Box>
-          <ContentFeed />
-        </Box>
+        <ResponsiveDrawer />
       </Container>
     </div>
   );
